5c1fc431c91eea5a2b3dce45df9e37b4e5f5dcb8
dani54196 <dani54196@gmail.com>
2022-06-29 12:50:44 -0300

    Updates

---
+++ component---src-pages-404-js-85c34e45cba95395419a.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_solidstate=self.webpackChunkgatsby_starter_solidstate||[]).push([[883],{429:function(e,t,a){a.r(t);var n=a(7294),l=a(9103);t.default=function(){return n.createElement(l.Z,{fullMenu:!0},n.createElement("section",{id:"wrapper"},n.createElement("header",null,n.createElement("div",{className:"inner"},n.createElement("h2",null,"Page not found"),n.createElement("p",null,"Not a valid URL")))))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-85c34e45cba95395419a.js.map
