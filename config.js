40bad03f617e7ebdb579182ebf1958fe18426ad8
dani54196 <dani54196@gmail.com>
2022-06-28 23:57:38 -0300

    Header and footer

---
+++ config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  siteTitle: 'Software of South', // <title>
+  manifestName: 'SolidState',
+  manifestShortName: 'SoS', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#0df8d8',
+  manifestThemeColor: '#0df8d8',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/gatsby-starter-solidstate/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'Software of South',
+  subHeading: 'Desarrollo de software, WEB, Backend, SRE, Cloud',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/dani54196',
+    },
+    {
+      icon: 'fa-whatsapp',
+      name: 'Whatsapp',
+      url: 'https://api.whatsapp.com/send?phone=5492612411868',
+    },
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/dani54196/',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:dani54196@gmail.com',
+    },
+  ],
+  phone: '+5492612411868',
+  address: 'Buenos Aires, Argentina',
+};
